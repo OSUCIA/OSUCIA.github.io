72717b7d2d119191dec4a228c699eb093b01725f
Matthew Whitlock <MatthewWhitlock.MW@gmail.com>
2017-09-04 13:53:50 -0500

    Auto update of webpage based on main git.

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #Updates the git repository with the club's website based on the files in the
 #   main repository
-cd Club #Move to the club's main Git folder
-git pull #Pull most recent files
+cd ../Club #Move to the club's main Git folder
+git fetch #Fetch most recent update info
 git reset --hard origin #Verify files are only what is contained in the git.
 
 for file in *.md; do
-  mv -f "$file" ../pages/"$file"
+  mv -f "$file" ../jekyll/pages/"$file"
 done
 
 cd Minutes
-rm ../../_posts/*.md #Remove all currently shown - to make sure deleted/renamed files aren't displayed.
+rm ../../jekyll/_posts/*.md #Remove all currently shown - to make sure deleted/renamed files aren't displayed.
 for file in *.md; do
-  mv -f "$file" ../../_posts/"$file"
+  mv -f "$file" ../../jekyll/_posts/"$file"
 done
 
-cd ../../pages
+cd ../../jekyll/pages
 mv README.md ../index.md
-rm "Fall Agenda.md"
 
 cd ..
 jekyll build #Build the site using config defaults
